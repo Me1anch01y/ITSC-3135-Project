088a601b9d39ee7176afb5983f7a49961635a83c
Me1anch01y <johnksmith8@gmail.com>
2024-04-23 23:26:40 -0400

    move html files

--- scripts/history.js
+++ scripts/history.js
@@ -1,17 +1,21 @@
 $(function () {
-  $("section").accordion({
-    collapsible: true,
-    active: false,
-    heightStyle: "content",
+  $("#accordion").accordion({
+    collapsible: true, // Allow all sections to be collapsed
+    active: false, // Initially, no section is active
+    heightStyle: "content", // Set height style to adjust according to content
     activate: function (event, ui) {
       if (ui.newPanel.length) {
         $("html, body").animate(
           {
-            scrollTop: ui.newPanel.offset().top - 100,
+            scrollTop: ui.newPanel.offset().top - 150,
           },
           "slow"
         );
       }
+    },
+    animate: {
+      duration: 950, // Set animation duration in milliseconds
+      easing: "easeInOutCubic", // Set easing function
     },
   });
 });
